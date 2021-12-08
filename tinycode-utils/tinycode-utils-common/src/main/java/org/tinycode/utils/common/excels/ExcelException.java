10d6eadf5c4cd0ed78acc4a555bd2860fb939490
littlehui <812699887@qq.com>
2021-12-08 11:34:20 +0800

    新增拼音工具

---
+++ tinycode-utils/tinycode-utils-common/src/main/java/org/tinycode/utils/common/excels/ExcelException.java
@@ -0,0 +1,14 @@
+package org.tinycode.utils.common.excels;
+
+/**
+ * TODO 
+ * @author littlehui
+ * @date 2021/12/6 16:48
+ * @version 1.0
+ */
+public class ExcelException extends Exception {
+
+    public ExcelException(String s) {
+        super(s);
+    }
+}
