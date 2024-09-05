3f51b04e7974f98137d6f443bf25cc784f220551
陈小辉 <littlehui@chenxiaohuideMacBook-Air.local>
2024-09-05 16:55:26 +0800

    升级sprin-boot到2.7.2

--- tinycode-web/tinycode-test-web/src/main/java/org/tinycode/web/mock/controller/ControllerLogTest.java
+++ tinycode-web/tinycode-test-web/src/main/java/org/tinycode/web/mock/controller/ControllerLogTest.java
@@ -3,11 +3,6 @@ package org.tinycode.web.mock.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * TODO
